824a9ab8c96fd4de087845e21fd31a67a726af4a
NyikoMnisii <nyikodeartkid@gmail.com>
2026-05-10 12:38:44 +0200

    deployment

--- Backend/models.py
+++ Backend/models.py
@@ -1,6 +1,6 @@
 from sqlalchemy import Column, Integer, String, Date, DateTime, Boolean
 from sqlalchemy.sql import func
-from .database import Base
+from database import Base
 
 class Staff(Base):
     __tablename__ = "staff"
